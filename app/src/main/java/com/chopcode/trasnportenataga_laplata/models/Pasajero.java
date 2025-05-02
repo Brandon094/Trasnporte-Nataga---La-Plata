b3e71c2fd5ca73e4ffbd0eb5a0eab59ada4a5ab2
Brandon094 <dazace94@gmail.com>
2025-05-02 11:01:48 -0500

    Update Pasajero.java

--- app/src/main/java/com/chopcode/trasnportenataga_laplata/models/Pasajero.java
+++ app/src/main/java/com/chopcode/trasnportenataga_laplata/models/Pasajero.java
@@ -2,14 +2,24 @@ package com.chopcode.trasnportenataga_laplata.models;
 
 public class Pasajero extends Usuario {
 
-    // Si en el futuro agregas atributos específicos para pasajeros, los defines aquí.
+    String rol = "pasajero";
 
     public Pasajero() {
         super();
     }
 
-    public Pasajero(String id, String nombre, String telefono, String email, String rol) {
-        super(id, nombre, telefono, email);
+    public Pasajero(String id, String nombre, String telefono, String email, String rol, String password) {
+        super(id, nombre, telefono, email, password);
+    }
+
+    @Override
+    public String toString() {
+        return "Pasajero: "+
+                "id=" + id +
+                "nombre=" + nombre +
+                "telefono=" + telefono +
+                "email=" + email +
+                "rol=" + rol;
     }
 
     // Métodos específicos para pasajeros, por ejemplo, para reservar asientos, pueden agregarse aquí.
